43c9c746132c3d0518ba5f04479343a1011fa73d
Aranyo Ray <aranyoray@gmail.com>
2024-07-04 13:45:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,37 +1,34 @@
 from flask import Flask, render_template, request
+from datetime import datetime
 from selenium import webdriver
+from selenium_stealth import stealth
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
-from datetime import datetime
 import time, logging, os, urllib.request
 
 app = Flask(__name__)
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-URL = "http://115.246.138.10/aktiv/Home.aspx"
-chrome_path = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
-options = webdriver.ChromeOptions()
-options.binary_location = chrome_path
+URL = "http://115.246.138.10/aktiv"
+options = Options()
 options.add_argument("--disable-extensions")
 options.add_argument("--disable-gpu")
-options.add_argument("--no-sandbox")
 options.add_argument("--headless=new")
+options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--remote-debugging-port=9222")
 
-# Configure Chrome options to handle PDF downloads
 options.add_experimental_option('prefs', {
     "download.default_directory": "/Users/aranyoray/Downloads",  # Change default directory for downloads
     "download.prompt_for_download": False,  # To auto-download the file
     "download.directory_upgrade": True,
     "plugins.always_open_pdf_externally": True  # It will not show PDF directly in Chrome
 })
-
-service = Service(ChromeDriverManager().install())
 
 def enter_username(driver):
     try:
@@ -63,13 +60,20 @@ def click_login_button(driver):
 def download_pdf(from_date, to_date):
     driver = None
     try:
-        driver = webdriver.Chrome(service=service, options=options)
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+        stealth(driver,
+                languages=["en-US", "en"],
+                vendor="Google Inc.",
+                platform="Win32",
+                webgl_vendor="Intel Inc.",
+                renderer="Intel Iris OpenGL Engine",
+                fix_hairline=True,)
         driver.get(URL)
         enter_username(driver)
         enter_password(driver)
         click_login_button(driver)
         
-        driver.get(f'http://115.246.138.10/aktiv/ReportBillRegisterDeptPrint.aspx?FD={from_date}&TD={to_date}&SB=1&BDTLS=1&SDA=0&SBS=0&GBSD=0&GBM=0&GBD=0&CBR=0&CPIT=0&Z=0&Z_TEXT=&U=0&U_TEXT=&SHAR=0&B=1&B_TEXT=ANUBHAV%20LIFE%20CARE%20,2/8%20JESSORE%20ROAD%20(EAST),BANAMALI%20PUR,GANJI%20MILL%20&RD=0&RD_TEXT=&ORG=0&ORG_TEXT=&COLL=0&COLL_TEXT=&DEPTSTR=&SUBDEPTSTR=')
+        driver.get(f'{URL}/ReportBillRegisterDeptPrint.aspx?FD={from_date}&TD={to_date}&SB=1&BDTLS=1&SDA=0&SBS=0&GBSD=0&GBM=0&GBD=0&CBR=0&CPIT=0&Z=0&Z_TEXT=&U=0&U_TEXT=&SHAR=0&B=1&B_TEXT=ANUBHAV%20LIFE%20CARE%20,2/8%20JESSORE%20ROAD%20(EAST),BANAMALI%20PUR,GANJI%20MILL%20&RD=0&RD_TEXT=&ORG=0&ORG_TEXT=&COLL=0&COLL_TEXT=&DEPTSTR=&SUBDEPTSTR=')
         try:   
             open_button = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "open-button")))
             open_button.click()
